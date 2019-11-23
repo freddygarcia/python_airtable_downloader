28ea78b3fabd85aabf2c4301220be9a35764e90b
west lepor <westlepor984@gmail.com>
2019-11-23 06:16:58 +0200

    call fun in generate.py

--- web.py
+++ web.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
+from generate import createXlsx
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
+    createXlsx()
     return render_template('index.html', url='https://www.test.com')
 
 if __name__ == '__main__':
