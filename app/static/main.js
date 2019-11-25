8757d6a1ee9a3b59aa9eb5ea033f69563eab7ca6
Freddy <buscando_alink@hotmail.com>
2019-11-25 15:05:37 -0400

    Add handle multiple export formats

--- app/static/main.js
+++ app/static/main.js
@@ -19,31 +19,42 @@ function clean() {
 
 function successResponse(response) {
 
-    var list_group = $('.list-group');
-    var arr = []
+    var file_list_group = $('.file-list-group');
 
-    for (document_id in response) {
-        var document = response[document_id];
-        arr.push({ 'name': document, id: document_id })
+    for (document_type in response) {
+        var documents = response[document_type];
+        var list_group = $(
+            '<div class="list-group list-group-flush my-5"> \
+                <a class="list-group-item list-group-item-secondary">' + document_type + '</a>\
+            </div>');
+
+        var arr = []
+
+        for (document_id in documents) {
+            var doc = documents[document_id];
+            arr.push({ 'name': doc.name, id: document_id })
+        }
+
+        // sort array by filename 
+        arr = arr.sort(function (a, b) {
+            if (a.name > b.name) {
+                return -1;
+            }
+            if (b.name > a.name) {
+                return 1;
+            }
+            return 0;
+        });
+
+        arr.forEach(function (doc) {
+            list_group.append($(
+                '<a href="/file/' + doc.id + '" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center ">' + doc.name +
+                    '<span class="badge badge-secondary badge-pill"><i class="fas fa-download"></i></span>' +
+                '</a>'
+            ))
+        })
+        file_list_group.append(list_group);
     }
-
-    arr = arr.sort(function (a, b) {
-        if (a.name > b.name) {
-            return -1;
-        }
-        if (b.name > a.name) {
-            return 1;
-        }
-        return 0;
-    });
-    arr.forEach(function (document) {
-        list_group.append($(
-            '<a href="/file/' + document.id + '" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">' + document.name +
-                '<span class="badge badge-primary badge-pill"><i class="fas fa-download"></i></span>' +
-            '</a>'
-        )
-        )
-    })
 }
 
 function badResponse(response) {
@@ -53,10 +64,10 @@ function badResponse(response) {
     var section = $('section');
     var alert = $(
         '<div class="alert-panel col-md-12 mt-5">' +
-            '<div class="alert alert-warning" role="alert">' +
-                '<strong class="">Code: ' + response.code + '</strong> ' +
-                text +
-            '</div>' +
+        '<div class="alert alert-warning" role="alert">' +
+        '<strong class="">Code: ' + response.code + '</strong> ' +
+        text +
+        '</div>' +
         '</div>'
     )
     section.append(alert);
